6a2111b0f53859d5085a65051ad72fbbc4769e12
Torontow <mtorontow0@gmail.com>
2020-10-18 12:12:54 -0400

    Adding variables and CSS file
    Forgot to add the provided CSS file on first commit; created variables for JS file

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// Assignment code
+// Generator Variables
 
 var generateBtn = document.querySelector("#generate");
+// The user will choose some or all of these types of characters for their password.
+var numString = ["0123456789"];
+var alphaString = ["abcdefghijklmnopqrstuvqxyz"];
+var specCharString = ["!#$%&()*+,-./:;<=>?@[^_`{|}~"];
+// User chooses type(s) of characters for their password;
+// those characters will be stored in _pCharString_.
+var pCharString = []; 
+// The function will choose _pLength_ characters at random from pCharString,
+// then push them to passwordText.
+var passwordText = []; 
+
+var pLength = prompt("Choose a number of characters (between 8 and 128).");
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -8,6 +23,16 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+ 
+
+ 
+    
+
+    }
+  
+
+
 
 }
 
