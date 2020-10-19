1d7c13d5bb0c84e7bee3f427f0f1cf72c4ba2e56
Torontow <mtorontow0@gmail.com>
2020-10-19 16:56:49 -0400

    Added Random Method
    Built a for-loop that will randomly retrieve the characters for the password from a string containing those characters, as stipulated by the user.

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // GENERATOR VARIABLES
 
-// generateBtn is the Generate Password button variable. It has an index of "generate".
+//  generateBtn is the Generate Password button variable. It has an index of "generate".
 var generateBtn = document.querySelector("#generate");
-// Makes the clicking the Generate Password button execute the _writePassword_ function
+//  Makes the clicking the Generate Password button execute the _writePassword_ function
 generateBtn.addEventListener("click", writePassword);
 
-// The user will choose some or all of these types of characters for their password.
+//  The user will choose some or all of these types of characters for their password.
 var numString = ["0123456789"];
 var alphaString = ["abcdefghijklmnopqrstuvqxyz"];
 var specCharString = ["!#$%&()*+,-./:;<=>?@[^_`|~"];
 
-// User chooses type(s) of characters for their password;
-// those characters will be stored in _pCharString_.
-var pCharString = []; 
-// The function will choose _pLength_ characters at random from pCharString,
+//  The function will choose _pLength_ characters at random from pCharString,
 // then push them to passwordText.
 var passwordText = []; 
 
@@ -31,13 +28,19 @@ function generatePassword() {
 
   // sets the length of the password (determined by user input)
   var pLength = prompt("Choose a number of characters (between 8 and 128).");
-  console.log(pLength);
-  // THIS SECTION WORKS: Decides which types of characters will make up the password and assigns a boolean  
+  var pCharString = ("lkjsndlfjonwenklsfjld");
+//  Decides which types of characters will make up the password and assigns a boolean  
   var num = confirm("Numbers?");
   var lCase = confirm("Lowercase letters?");
   var uCase = confirm("Uppercase?");
   var specChar = confirm("Special Characters?");
-  // 
+
+
+// This finds a random character from the string of chosen password characters
+// and adds it to the password text array.
+for (var i=0; i<pLength; i++) {
+  passwordText.push(pCharString[Math.floor(Math.random()*pCharString.length)]);
+}
 
 
 }
