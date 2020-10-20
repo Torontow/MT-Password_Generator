63a6634a070dc1d744cdde496dafa7cb6742a181
Torontow <mtorontow0@gmail.com>
2020-10-20 16:17:30 -0400

    Minor editions
    Proofread and rewrote prompt and alert text for consistency of style. Cleaned up indentation and spacing of code.

--- script.js
+++ script.js
@@ -1,76 +1,73 @@
-// GENERATOR VARIABLES
-
-//  generateBtn is the Generate Password button variable. It has an index of "generate".
+// Makes the "Generate Password" button execute writePassword() when clicked.
 var generateBtn = document.querySelector("#generate");
-//  Makes the clicking the Generate Password button execute the _writePassword_ function
 generateBtn.addEventListener("click", writePassword);
 
-//  The user will choose some or all of these types of characters for their password.
+
+// Stores the character sets user will choose from to build the password.  
 var numString = ["0123456789"];
 var lAlphaString = ["abcdefghijklmnopqrstuvqxyz"];
 var uAlphaString = ["ABCDEFGHIJKLMNOPQRSTUVQXYZ"]
 var specCharString = ["!#$%&()*+,-./:;<=>?@[^_`|~"];
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  // Write password to the #password input
-  passwordText.value = password;
+// Replaces "Your Secure Password" in document with user-defined password.
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    // Write password to the #password input field
+    passwordText.value = password;
 }
 
-function generatePassword() {
-  
-  // Restricts users to choosing a password between 8 and 128 characters.
-  var pLength = 0;
-  while (pLength < 8 || pLength > 128) {
-    pLength = prompt("Enter desired number of characters for password between 8 and 128.");
+// Creates password with criteria defined by user for use in writePassword().
+function generatePassword() {  
+    // Restricts users to choosing a length between 8 and 128 characters.
+    var pLength = 0;
+    while (pLength < 8 || pLength > 128) {
+      pLength = prompt("Enter desired number of characters for password between 8 and 128.");
     }
 
-      // User chooses which types of characters will make up the password and assigns a boolean  
-      var num = confirm("Click OK to include integers.");
-      var lCase = confirm("Click OK to include lowercase letters.");
-      var uCase = confirm("Click OK to include uppercase letters.");
-      var specChar = confirm("Click OK to include special characters.");
+    // User chooses which character sets to include in password. 
+    var num = confirm("Click OK to include integers (0 through 9).");
+    var lCase = confirm("Click OK to include lowercase letters.");
+    var uCase = confirm("Click OK to include UPPERCASE letters.");
+    var specChar = confirm("Click OK to include special characters (!@#$, etc.)");
 
-      // Defines array of types of characters the user wants for their password.
-      var pCharString = [];
-
-      // Adds whichever types of characters users confirm for their password
-      // and pushes them to a storage place
-      if (num) {
-        pCharString.push(numString);
-      }
-      if (lCase) {
-        pCharString.push(lAlphaString);
-      }
-      if (uCase) {
-        pCharString.push(uAlphaString);
-      }
-      if (specChar) {
-        pCharString.push(specCharString);
-      } 
-      
-      // Validates that user chose at least one type of character to use.
-      switch (pCharString.length) {
-        case 0:
-          alert("Error: No valid characters included.")
-      }       
-      
-      // Combines all the character types chosen and creates a single string
-      // which will be drawn from to create the password
-      var pChars = pCharString.join("");
-      // Creates an empty array to store final password text characters
-      var passwordTextArray = [];
-
-      // Chooses characters at random from the string of character types 
-      // and adds them to the final password text array.
-      for (var i=0; i<pLength; i++) {
-        passwordTextArray.push(pChars[Math.floor(Math.random()*pChars.length)]);
-      }
-      // Returns the final array as a single string to the document
-      return (passwordTextArray.join(""));
+    // Builds the array that contains the user-selected character sets.
+    var pCharString = [];
+    if (num) {
+      pCharString.push(numString);
+    }
+    if (lCase) {
+      pCharString.push(lAlphaString);
+    }
+    if (uCase) {
+      pCharString.push(uAlphaString);
+    }
+    if (specChar) {
+      pCharString.push(specCharString);
+    } 
     
+    // Validates that user chose at least one character set to use.
+    switch (pCharString.length) {
+      case 0:
+        alert("Error: No characters were selected to include in your password. Please try again.")
+    }       
+    
+    // Combines chosen character sets to create a single string
+    // of allowable password characters.
+    var pChars = pCharString.join("");
+
+    // Defines an array to store password text characters.
+    var passwordTextArray = [];
+
+    // Selects the final password characters and 
+    // stores them in the passwordTextArray array.
+    for (var i=0; i<pLength; i++) {
+      passwordTextArray.push(pChars[Math.floor(Math.random()*pChars.length)]);
+    }
+    // Returns the passwordTextArray as a single string to writePassword().  
+    return (passwordTextArray.join(""));
 }
 
 
